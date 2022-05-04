dcf85727a19d8e88e3ba99547d71c78af25166d5
sorious77 <sorious77@naver.com>
2022-05-05 00:11:27 +0900

    Firebase 연동

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import {useState} from "react";
+import { useEffect, useState } from "react";
+import { fireStore } from "./Firebase";
 
 function App() {
-const [subwayStation, setSubwayStation] = useState("");
+  const [subwayStation, setSubwayStation] = useState("");
 
-const handleOnClick = () => {
-  console.log("hi")
-  setSubwayStation("new Station");
-}
+  const handleOnClick = () => {
+    console.log("hi");
+    setSubwayStation("new Station");
+  };
+
+  useEffect(() => {
+    console.log(fireStore);
+  });
 
   return (
     <div className="App">
       <button onClick={handleOnClick}>클릭</button>
       <div>{subwayStation}</div>
+      {fireStore._databaseId.projectId}
     </div>
   );
 }
